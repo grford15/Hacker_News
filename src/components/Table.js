a6618a69c0c9c2cb8a3f626d08b402f90cd449f3
Greg <g.rutherford10@gmail.com>
2020-01-07 09:48:35 +0000

    add some basic styling to app

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,17 +5,20 @@ const isSearched = searchTerm => item =>
 
 const Table = ({ list, pattern, onDismiss }) => {
   return (
-    <div>
+    <div className="table">
       {list.filter(isSearched(pattern)).map(item => (
-        <div key={item.objectID}>
-          <span>
+        <div key={item.objectID} className="table-row">
+          <span style={{ width: '40%' }}>
             <a href={item.url}>{item.title}</a>
           </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <Button onClick={() => onDismiss(item.objectID)}>
+          <span className="large-column">{item.author}</span>
+          <span className="medium-column">{item.num_comments}</span>
+          <span className="small-column">{item.points}</span>
+          <span className="small-column">
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
+            >
               Dismiss
             </Button>
           </span>
